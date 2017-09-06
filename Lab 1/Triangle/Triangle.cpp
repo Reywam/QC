eaa741d3d2b1db562f73a21fd892e2dd5ae876a5
Reywam <reywam@mail.ru>
2017-09-06 14:41:05 +0300

    Добавлено задание первой лабораторной работы

---
+++ Lab 1/Triangle/Triangle.cpp
@@ -0,0 +1,91 @@
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+bool IsValidNumber(const string &number)
+{
+	bool result = true;
+
+	for (size_t i = 0; i < number.size(); i++)
+	{
+		if (number[i] <= '0' || number[i] > '9')
+		{			
+			result = false;
+		}
+	}
+	return result;
+}
+
+bool ValidInput(char* argv[])
+{
+	string a = argv[1];
+	string b = argv[2];
+	string c = argv[3];
+	
+	bool result = true;
+
+	if (!IsValidNumber(a)
+		|| !IsValidNumber(b)
+		|| !IsValidNumber(c))
+	{
+		result = false;
+	}
+	return result;
+}
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "Rus");
+
+	if (argc != 4)
+	{
+		cout << "Неверное количество аргументов. Формат ввода: Triangle.exe 5 5 5" << endl;
+		return 1;
+	}
+
+	unsigned int a, b, c;
+
+	if (!ValidInput(argv))
+	{
+		cout << "Ошибка ввода. Формат ввода: Triangle.exe 5 5 5" << endl;
+		cout << "Допустимы только положительные числа" << endl;
+		return 1;
+	}
+	
+	a = stoi(argv[1]);
+	b = stoi(argv[2]);
+	c = stoi(argv[3]);
+
+	// Отрезки образуют треугольник если самый больший меньше суммы двух других	
+	unsigned int maxLine = max(a, b);
+	maxLine = max(maxLine, c);
+
+	unsigned const int twoLinesSum = a + b + c - maxLine;
+
+	if (maxLine >= twoLinesSum)
+	{
+		cout << "Фигура не является треугольником" << endl;
+		return 1;
+	}
+		
+	string result;
+	if (a == b && b == c && c == a)
+	{
+		result = "Треугольник равносторонний";
+	}
+	else if (a == b || b == c || a == c)
+	{
+		result = "Треугольник равнобедренный";
+	}	
+	else
+	{
+		result = "Обычный треугольник";
+	}
+
+	cout << result << endl;
+    return 0;
+}
